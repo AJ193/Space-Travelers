6a18bb80b0fe88c64d22471743717703aa5d092d
yemidada <yemidada@gmail.com>
2023-07-26 07:57:06 +0100

    Update dragon component

--- src/components/Dragon.js
+++ src/components/Dragon.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { getDragons } from '../redux/dragons/dragonsSlice';
+import { getDragons, updateDragon } from '../redux/dragons/dragonsSlice';
 
 const Dragon = () => {
   const dispatch = useDispatch();
@@ -10,7 +10,12 @@ const Dragon = () => {
 
   const { dragons, loading } = useSelector((state) => state.dragons);
 
+  const onUpdate = (id) => {
+    dispatch(updateDragon(id));
+  };
+
   if (loading) return <p>Loading...</p>;
+
   return (
     <div className="container mt-5 dragon">
       <div className="list">
@@ -20,7 +25,11 @@ const Dragon = () => {
             <div className="col-md-9">
               <h2 className="title">{dragon.name}</h2>
               <p className="title">{dragon.type}</p>
-              <button type="button" className="btn btn-primary">Reserve Rocket</button>
+              {
+                dragon.reserved ? <button type="button" className="btn btn-outline-primary">Cancel Reservation</button>
+                  : <button onClick={() => onUpdate(dragon.id)} type="button" className="btn btn-primary">Reserve Rocket</button>
+              }
+
             </div>
           </div>
         ))}
