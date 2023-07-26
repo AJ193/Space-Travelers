b1ae0630988d0d3e089229e73874ea1305f70dc1
Adeyemi Dada <yemidada@gmail.com>
2023-07-26 08:43:58 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
+import Dragon from './components/Dragon';
 
 function App() {
   return (
